40aa978679a80e3b62160be021cbed5737129e1c
Gabriel Tinoco <gabrielslai@gmail.com>
2023-03-21 22:12:52 -0300

    Added bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
   return <Component {...pageProps} />
 }
