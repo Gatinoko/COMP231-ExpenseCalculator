c373223d5227ddf5c377b2a9c4f2f24df19ce2db
Gabriel Tinoco <gabrielslai@gmail.com>
2023-03-23 16:32:25 -0300

    Changed Navigation component location

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
-import '@/styles/globals.css'
+// CSS imports
 import 'bootstrap/dist/css/bootstrap.css'
+import '@/styles/globals.css'
+
+// Next/React imports
+import Head from 'next/head'
 import { useEffect } from 'react'
+
+// Component imports
+import Navigation from '@/Components/navigation'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    import('bootstrap/dist/js/bootstrap')
+    require('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, [])
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>Expense Calculator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navigation />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Navigation from '@/Components/navigation'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Navigation />
         <Main />
         <NextScript />
       </body>
