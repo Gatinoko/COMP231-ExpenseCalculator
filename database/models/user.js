0b20362c83f6449f0702b4592ae914953e53800f
Gabriel Tinoco <gabrielslai@gmail.com>
2023-03-24 15:20:28 -0300

    Implemented User data model

--- database/models/user.js
+++ database/models/user.js
@@ -1,3 +1,40 @@
-/*
-    User data model implementation goes here
-*/
+const mongoose = require('mongoose')
+
+// Not yet implemented into schema
+const Expenses = {
+        expenseName: String,
+        expenseCost: Number,
+        expenseDate: Date
+};
+
+// User schema
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    expenses: Array
+}, 
+{
+    collection: 'users'
+});
+
+// Method to validate user password on login
+UserSchema.methods.isValidPassword = async (password) => {
+    const user = this
+    const compare = (password === user.password)
+    return compare
+}
+
+Users = mongoose.model('user', UserSchema);
+module.exports = Users;
