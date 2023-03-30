85410907b5ae975870dca1ced848afe9128e0ab7
Gabriel Tinoco <gabrielslai@gmail.com>
2023-03-30 13:51:43 -0300

    Implemented role verification middleware

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -1,3 +1,10 @@
+const cookieHelper = require('../utils/cookieHelper'),
+{ userRoles, userRolesKeys } = require('../../database/resources/userRoles'),
+jwt = require('jsonwebtoken')
+
+/*
+    Middleware
+*/
 exports.loginMiddleware = async (req, res, next) => {
     const email = req.body.email
     const password = req.body.password
@@ -5,18 +12,44 @@ exports.loginMiddleware = async (req, res, next) => {
     const user = await Users.findOne({ email })
     if (user === null) {
         req.valid = false
-        req.message = 'Email not registered'
+        req.loginMessage = 'Email not registered'
         next() 
     }
     else if (!user.isValidPassword(password)) {
         req.valid = false
-        req.message = 'Wrong password'
+        req.loginMessage = 'Wrong password'
         next()
     }
     else {
         req.valid = true
         req.user = user
-        req.message = 'Successfully logged in'
+        req.loginMessage = 'Successfully logged in'
         next()
     }
+}
+
+exports.verifyUserMiddleware = async (req, res, next) => {
+    const allowedRoles = [ userRoles.Admin ]
+    verifyRolesMiddleware(req, res, next, allowedRoles)
+}
+
+/*
+    Helper functions
+*/
+verifyRolesMiddleware = async (req, res, next, allowedRoles) => {
+    const token = cookieHelper.geReqCookieValueByName(req, 'token')
+    const { roles } = jwt.decode(token)
+    const containsRoles = roles.every(role => {
+        return allowedRoles.includes(role)
+    })
+    if (containsRoles === true) {
+        req.validRoles = true
+        next()
+    }
+    else {
+        req.validRoles = false
+        req.verifyRolesMessage = 'Not enough roles'
+        next()
+    }
+    
 }
