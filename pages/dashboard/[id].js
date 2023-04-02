f56ead18fefabc624600678023ab6dae1012512c
Gabriel Tinoco <gabrielslai@gmail.com>
2023-04-02 15:59:25 -0300

    Finalized the sidebar styling

--- pages/dashboard/[id].js
+++ pages/dashboard/[id].js
@@ -1,3 +1,6 @@
+// CSS imports
+import styles from "./dashboard.module.css"
+
 // Component imports
 import ExpenseGroup from "@/Components/expenseDashboard/expenseGroup/expenseGroup";
 import ExpenseCard from "@/Components/expenseDashboard/expenseCard/expenseCard";
@@ -19,55 +22,101 @@ export default function Dashboard() {
 
                 {/* Sidebar navigation */}
                 <nav className="d-flex
-                                bg-light
+                                bg-dark
                                 shadow-sm
                                 flex-column
-                                p-3">  
+                                p-3" style={{width: '300px'}}>  
 
                     {/* Item list */}
                     <ul class="nav 
-                               flex-column">
+                               flex-column gap-4">
 
-                        {/* Expenses */}
-                        <li class="nav-item">
-                            <a class="nav-link 
-                                      d-flex 
-                                      gap-2 
-                                      align-items-center" 
-                                aria-current="page" 
-                                href="#">
+                        {/* Pages section */}
+                        <div className="d-flex
+                                        flex-column
+                                        gap-1">
+
+                            {/* Nav section name */}
+                            <h6 className="border-bottom
+                                           text-light 
+                                           pb-2">
+                                Pages
+                            </h6>
+
+                            {/* Expenses */}
+                            <li class="nav-item">
+                                <a class="btn 
+                                          btn-dark
+                                          d-flex
+                                          gap-2 
+                                          align-items-center" 
+                                    aria-current="page" 
+                                    href="#">
+                                        
+                                    <i class="fa-solid fa-house"/>
+
+                                    Expenses
+                                </a>
+                            </li>
+
+                            {/* Statistics */}
+                            <li class="nav-item">
+                                <a class="btn 
+                                          btn-dark 
+                                          d-flex
+                                          gap-2 
+                                          align-items-center" 
+                                    href="#">
+
+                                    <i class="fa-solid fa-chart-simple"/>
                                     
-                                <i class="fa-solid fa-house"/>
+                                    Statistics
+                                </a>
+                            </li>
 
-                                Expenses
-                            </a>
-                        </li>
+                            {/* Reports */}
+                            <li class="nav-item">
+                                <a class="btn 
+                                          btn-dark 
+                                          d-flex
+                                          gap-2 
+                                          align-items-center" 
+                                    href="#">
 
-                        {/* Statistics */}
-                        <li class="nav-item">
-                            <a class="nav-link 
-                                      d-flex 
-                                      gap-2 
-                                      align-items-center" 
-                                href="#">
+                                    <i class="fa-solid fa-file-lines"/>
 
-                                <i class="fa-solid fa-chart-simple"/>
-                                
-                                Statistics
-                            </a>
-                        </li>
+                                    Reports
+                                </a>
+                            </li>
+                        </div>
 
-                        {/* Reports */}
-                        <li class="nav-item">
-                            <a class="nav-link 
-                                      d-flex 
-                                      gap-2 
-                                      align-items-center" 
-                                href="#">
-                                <i class="fa-solid fa-file-lines"/>
-                                Reports
-                            </a>
-                        </li>
+                        {/* Actions section */}
+                        <div className="d-flex
+                                        flex-column
+                                        gap-1">
+
+                            {/* Nav section name */}
+                            <h6 className="border-bottom
+                                           text-light 
+                                           pb-2">
+                                Actions
+                            </h6>
+
+                            {/* Add expense */}
+                            <li class="nav-item">
+                                <a class="btn 
+                                          btn-dark 
+                                          d-flex
+                                          gap-2 
+                                          align-items-center" 
+                                    href="#">
+
+                                    <i class="fa-solid fa-plus"/>
+
+                                    Add Expense
+                                </a>
+                            </li>
+                        </div>
                     </ul>
                 </nav>
 
@@ -104,12 +153,15 @@ export default function Dashboard() {
                                            btn-sm 
                                            btn-outline-secondary 
                                            dropdown-toggle">
+
                                 <i class="fa-solid fa-calendar pe-2"/>
+
                                 This week
                             </button>
                         </div>
                     </div>
 
+                        {/* Expense groups and expenses */}
                         <div className="d-flex 
                                         flex-row 
                                         w-100 
