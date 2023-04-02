9d54ce0c9f093b35ba1c87631e38f7914d242ea0
Gabriel Tinoco <gabrielslai@gmail.com>
2023-04-02 17:17:27 -0300

    Created initial ExpenseModal component

--- Components/expenseModal/expenseModal.js
+++ Components/expenseModal/expenseModal.js
@@ -4,66 +4,97 @@ import styles from './expenseModal.module.css'
 /*
     Expense modal component
 */
-export default function ExpenseModal() {
+export default function ExpenseModal({ modalId }) { 
     return(
         <>
-        <div className='container d-flex justify-content-center'>
-            <form className={`d-flex flex-column p-3 mx-5 shadow rounded ${styles['AddExpense-form']}`}>
+        <div class="modal fade" 
+             id={modalId} 
+             tabindex="-1" 
+             aria-labelledby="exampleModalLabel" 
+             aria-hidden="true">
+            
+            <div class="modal-dialog">
+                <div class="modal-content">
 
-                {/* User login header */}
-                <div className='mb-3'>
-                    <h1>Add Expense</h1>
+                    {/* Header */}
+                    <div class="modal-header">
+
+                        {/* User login header */}
+                        <h1>Add Expense</h1>
+
+                        {/* Close button */}
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    {/* Modal body */}
+                    <div class="modal-body">
+                        <form>
+
+                            {/* Expense name input field */}
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" 
+                                    className="form-label">Expense Name</label>
+                                <input type="text" 
+                                    className="form-control" 
+                                    id="exampleFormControlInput1" 
+                                    placeholder="Expense Name"
+                                    required />
+                            </div>
+
+                            {/* Expense type input field */}
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" 
+                                    className="form-label">Expense Type</label>
+                                <input type="text" 
+                                    className="form-control" 
+                                    id="exampleFormControlInput1" 
+                                    placeholder="Expense Type"
+                                    required />
+                            </div>
+
+                            {/* Expense cost input field */}
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" 
+                                    className="form-label">Expense Cost</label>
+                                <input type="text" 
+                                    className="form-control" 
+                                    id="exampleFormControlInput1" 
+                                    placeholder="Expense Cost"
+                                    required />
+                            </div>
+
+                            {/* Expense date input field */}
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" 
+                                    className="form-label">Expense Date</label>
+                                <input type="text" 
+                                    className="form-control" 
+                                    id="exampleFormControlInput1" 
+                                    placeholder="Expense Date"
+                                    required />
+                            </div>
+
+                            {/* Expense group input field */}
+                            <div className="mb-3">
+                                <label htmlFor="exampleFormControlInput1" 
+                                    className="form-label">Expense Group</label>
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+
+                    {/* Buttons */}
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Add Expense</button>
+                    </div>
                 </div>
-
-                {/* Expense Name input field */}
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" 
-                        className="form-label">Expense Name</label>
-                    <input type="text" 
-                        className="form-control" 
-                        id="exampleFormControlInput1" 
-                        placeholder="Expense Name"
-                        required />
-                </div>
-
-                {/* Expense Type input field */}
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" 
-                        className="form-label">Expense Type</label>
-                    <input type="text" 
-                        className="form-control" 
-                        id="exampleFormControlInput1" 
-                        placeholder="Expense Type"
-                        required />
-                </div>
-
-                {/* Expense Cost input field */}
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" 
-                        className="form-label">Expense Cost</label>
-                    <input type="text" 
-                        className="form-control" 
-                        id="exampleFormControlInput1" 
-                        placeholder="Expense Cost"
-                        required />
-                </div>
-
-                {/* Expense Date input field */}
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" 
-                        className="form-label">Expense Date</label>
-                    <input type="text" 
-                        className="form-control" 
-                        id="exampleFormControlInput1" 
-                        placeholder="Expense Date"
-                        required />
-                </div>
-
-                {/* Login button */}
-                <button type="submit" 
-                        className="btn btn-light align-self-center mb-3">Add Expense</button>
-                
-            </form>
+            </div>
         </div>
         </>
     )
