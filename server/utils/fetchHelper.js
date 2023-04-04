1e34ef8ae84167f4e467a281aa01fd0f150472a9
Gabriel Tinoco <gabrielslai@gmail.com>
2023-04-04 09:58:35 -0300

    Changed fetch route

--- server/utils/fetchHelper.js
+++ server/utils/fetchHelper.js
@@ -2,14 +2,14 @@ const BASE_URL = 'http://localhost:3000'
 
 // Get all users
 export const getAllUsers = async () => {
-    const res = await fetch(`${BASE_URL}/api/users`);
+    const res = await fetch(`${BASE_URL}/api/get/users`);
     const json = await res.json();
     return json;
 };
 
 // Get user
 export const getUser = async (_id) => {
-    const res = await fetch(`${BASE_URL}/api/users/${_id}`);
+    const res = await fetch(`${BASE_URL}/api/get/users/${_id}`);
     const json = await res.json();
     if (json) return json;
     else return {};
